0091bc32b214dca559a70ff5cd47ffc5779f0478
Eduardo <eduardocl@gmail.com>
2014-07-05 15:34:03 -0300

    Update feed.py

--- feed.py
+++ feed.py
@@ -2,17 +2,8 @@
 __author__ = "Eduardo Lopes"
 __email__ = "eduardocl@gmail.com"
 
+#feeds urls
 feeds = [
-        ("Reinaldo Azevedo",    "http://feeds2.feedburner.com/ReinaldoAzevedo"), 
-        ("Felipe Moura Brasil", "http://veja.abril.com.br/blog/felipe-moura-brasil/feed/"),  
         ("Weekend Nerd",        "http://weekendnerd.wordpress.com/feed/"),
         ('Code Geeks',          'http://feeds.feedburner.com/JavaCodeGeeks'),
-        ('InfoQ',               'http://www.infoq.com/feed?token=qdCDlUtIrjIbwCqp02LknYnZ2abvhfLJ'),
-        (u'Mídia Sem Máscara',  'http://feeds.feedburner.com/midiasemmascara'),
-        ("American Thinker",    "http://feeds.feedburner.com/americanthinker_articles?format=xml"),
-        ("Notalatina",          "http://notalatina.blogspot.com/feeds/posts/default"),
-        ("Tomatadas",           "http://tomatadas.blogspot.com/feeds/posts/default"),
-        ("Luciano Ayan",        "http://lucianoayan.com/feed/"),
-        ("WND World",           "http://www.wnd.com/category/front-page/world/feed/"),
-        ("Mises Institute",     "http://www.mises.org.br/RSSArticles.aspx?type=1&culture=pt"),
         ("Le Gorafi",           "http://www.legorafi.fr/feed")]
