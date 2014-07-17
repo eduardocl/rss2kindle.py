baa03811398f31334eb0f2cf61d9498b153c0a9f
Eduardo <eduardocl@gmail.com>
2014-07-17 13:12:20 -0300

    manipulando xml

--- xmlparse.py
+++ xmlparse.py
@@ -1,34 +1,49 @@
-from xml.dom import minidom
 
-#import sys
-#reload(sys)
-#sys.setdefaultencoding("utf-8")  
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")  
 
-xml = minidom.parse('feeds.xml')
-
-feeds = xml.getElementsByTagName('feed')
-
-for f in feeds:
-    name = f.getElementsByTagName('name')[0].firstChild.nodeValue
-    #name = f.getElementsByTagName('url')[0].firstChild.nodeValue
-   
-    
-#inserindo um nodo    
-root = xml.getElementsByTagName('feeds')[0]
-feed = xml.createElement('feed')
-name = xml.createElement('name')
-namevalue = xml.createTextNode('nome do feed')
-name.appendChild(namevalue)
-feed.appendChild(name)
-root.appendChild(feed)
-print root
+from xml.etree.ElementTree import ElementTree as ET
+from xml.etree.ElementTree import parse
+from xml.etree.ElementTree import SubElement
+from xml.etree.ElementTree import Element
 
 import codecs
-filexml = codecs.open('feeds.xml','w', encoding="utf-8")
-xmlcontent = xml.toprettyxml()
-print xmlcontent
-import re
-text_re = re.compile('>\n\s+([^<>\s].*?)\n\s+</', re.DOTALL)    
-pretty_xml = text_re.sub('>\g<1></', xmlcontent)
-xml.writexml(filexml)
-filexml.close()
+
+input_xml = codecs.open('feeds.xml','r', encoding="utf-8")
+tree = parse(input_xml)
+root = tree.getroot()
+
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if elem is not None:
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+
+def add_feed(feedname, feedurl):
+    feed = SubElement(root, "feed")
+    name = SubElement(feed, "name")
+    name.text = feedname
+    url = SubElement(feed, "url")
+    url.text = feedurl
+
+
+add_feed("feed 1", "link 1")
+add_feed("feed 2", "link 2")
+add_feed("feed 3", "link 3")
+
+indent(root)
+tree.write("resultado.xml", encoding="utf-8")
+
+
