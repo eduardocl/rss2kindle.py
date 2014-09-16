4f5c4b05fe927c7c262ac8aaba66026208c49271
Eduardo <eduardocl@gmail.com>
2014-09-15 21:19:55 -0300

    Update feed.py

--- feed.py
+++ feed.py
@@ -6,7 +6,5 @@ __email__ = "eduardocl@gmail.com"
 feeds = [
         ("Weekend Nerd",        "http://weekendnerd.wordpress.com/feed/"),
         ("Le Gorafi",           "http://www.legorafi.fr/feed"),
-        ("Reinaldo Azevedo",    "http://veja.abril.com.br/blog/reinaldo/feed/"),
-        ("Felipe Moura Brasil", "http://veja.abril.com.br/blog/felipe-moura-brasil/feed/"),
         ("Daily Galaxy",        "http://www.dailygalaxy.com/my_weblog/rss.xml"),
         ("Akita on Rails",      "http://feeds.feedburner.com/AkitaOnRails")]
